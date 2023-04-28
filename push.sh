4e489716f5a61be997fc9bc1cbb15586830aa918
n0izestr3am <n0izestr3am@gmail.com>
2023-04-28 16:55:59 +0700

    |  2023-04-28-16:55:59

--- push.sh
+++ push.sh
@@ -2,11 +2,13 @@ git config --global user.email "n0izestr3am@gmail.com"
 git config --global user.name "n0izestr3am"
 #
 git add .
+
 echo 'Judul:'
 read commitMessage
 
-git commit -m "push update"
+git commit -a -m "$commitMessage |  `date +%F-%T`"
 #
-git push
-
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+git push origin "$current_branch"
+echo "===== '$current_branch' branch"
 read
