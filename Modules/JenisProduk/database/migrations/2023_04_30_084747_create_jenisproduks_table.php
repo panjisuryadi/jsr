1739bac0a6f1ddcbbfa8197c33563126b76e8f3a
n0izestr3am <n0izestr3am@gmail.com>
2023-04-30 21:18:30 +0700

    image jenis Produk |  2023-04-30-21:18:30

--- Modules/JenisProduk/database/migrations/2023_04_30_084747_create_jenisproduks_table.php
+++ Modules/JenisProduk/database/migrations/2023_04_30_084747_create_jenisproduks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            //$table->string('image')->nullable(true);
+            $table->string('image')->nullable(true);
             $table->timestamps();
 
         });
