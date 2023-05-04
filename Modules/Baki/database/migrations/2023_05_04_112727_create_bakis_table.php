0b4d3a485334af946750597096d6f077f78fec6c
n0izestr3am <n0izestr3am@gmail.com>
2023-05-04 23:55:17 +0700

    |  2023-05-04-23:55:17

--- Modules/Baki/database/migrations/2023_05_04_112727_create_bakis_table.php
+++ Modules/Baki/database/migrations/2023_05_04_112727_create_bakis_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('bakis');
         Schema::create('bakis', function (Blueprint $table) {
            $table->id();
             $table->unsignedBigInteger('gudang_id');
