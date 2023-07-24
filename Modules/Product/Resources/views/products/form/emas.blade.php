c960ca36966de32b775f217c9848769cb34a70ef
n0izestr3am <n0izestr3am@gmail.com>
2023-07-24 16:08:42 +0700

    |  2023-07-24-16:08:42

--- Modules/Product/Resources/views/products/form/emas.blade.php
+++ Modules/Product/Resources/views/products/form/emas.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 gap-2">
+<div class="grid grid-cols-2 gap-2">
     <div class="form-group">
         <label for="product_note">Kategori Emas</label>
         <select class="form-control select2" name="gold_kategori_id" id="gold_kategori_id" required>
@@ -9,4 +9,16 @@
             @endforeach
         </select>
     </div>
+
+ <div class="form-group">
+        <label for="karat_id">@lang('Karat') <span class="text-danger">*</span></label>
+         <select class="form-control select2" name="jenis_perhiasan_id" id="jenis_perhiasan_id" required>
+            <option value="" selected disabled>Karat</option>
+            @foreach(\Modules\Karat\Models\Karat::all() as $jp)
+            <option value="{{ $jp->id }}">{{ $jp->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+
 </div>
