17431dd519b150bb927abd1f570ca0d30defadc2
n0izestr3am <n0izestr3am@gmail.com>
2023-07-24 13:35:07 +0700

    |  2023-07-24-13:35:07

--- Modules/Product/Resources/views/products/sortir_dashboard.blade.php
+++ Modules/Product/Resources/views/products/sortir_dashboard.blade.php
@@ -15,6 +15,66 @@ display: inline-block;
 </style>
 
 
+<div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-value-lg">23</div>
+                        <div>Total Stok Opname</div>
+                        <div class="progress progress-xs my-2">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-value-lg" id="totalcategory">0</div>
+                        <div>Total Stok Opname</div>
+                        <div class="progress progress-xs my-2">
+                            <div class="progress-bar bg-primary" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+            <div class="col-md-4">
+                <a class="card" href="javascript:;" id="successadjustment">
+                    <div class="card-body text-dark">
+                        <div class="text-value-lg" id="totalaset">0</div>
+                        <div>Total Sortir</div>
+                        <div class="progress progress-xs my-2">
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a class="card" href="javascript:;" id="pendingadjustment">
+                    <div class="card-body">
+                        <div class="text-value-lg" id="totalproduct">0</div>
+                        <div>Total Approve</div>
+                        <div class="progress progress-xs my-2">
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   <div class="row">
             <div class="col-lg-12">
