3a5b23ef4b09f3928b614820b1cd16ae597c4bbf
n0izestr3am <n0izestr3am@gmail.com>
2023-08-24 16:14:16 +0700

    |  2023-08-24-16:14:16

--- Modules/GoodsReceipt/Resources/views/goodsreceipts/view_produk.blade.php
+++ Modules/GoodsReceipt/Resources/views/goodsreceipts/view_produk.blade.php
@@ -8,19 +8,32 @@
 
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                     <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                    {{ @$detail->code }}
+                    {{ @$detail->product_code }}
                     </h2>
                     <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ @$detail->product_name }}</h1>
-                    <div class="flex mb-4">
-                        <span class="flex items-center">
+                    <div class="mb-4">
+                        <div class="items-center">
                             <span class="text-gray-600 ml-1">Berat : {{ @$detail->berat_barang }}</span>
-                        </span>
+                        </div>
+                        <div class="items-center">
+                            <span class="text-gray-600 ml-1">Berat : {{ @$detail->berat_barang }}</span>
+                        </div>
 
                     </div>
-                    <p class="leading-relaxed text-gray-600">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
-                       <div class="flex">
-                    <span class="title-font font-medium text-2xl text-gray-900">
-                    {{ number_format($detail->product_price) }}</span>
+
+
+                    <div class="mb-4 mt-2 border-t">
+                        <div class="items-center">
+                            <span class="text-md text-gray-600 ml-1">Beli :  {{ number_format($detail->product_cost) }}</span>
+                        </div>
+                        <div class="items-center">
+                            <span class="text-md xtext-gray-600 ml-1">Jual :  {{ number_format($detail->product_price) }}</span>
+                        </div>
+
+                    </div>
+
+             <div class="flex">
+
                     <button class="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded">Print Barcode</button>
 
                 </div>
