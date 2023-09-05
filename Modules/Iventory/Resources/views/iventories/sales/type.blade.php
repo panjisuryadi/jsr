db0d404ec5841654dfa0605fbfd3c36df3eafb36
n0izestr3am <n0izestr3am@gmail.com>
2023-09-05 16:57:41 +0700

    module DISTRIBUSI SALES |  2023-09-05-16:57:41

--- Modules/Iventory/Resources/views/iventories/sales/type.blade.php
+++ Modules/Iventory/Resources/views/iventories/sales/type.blade.php
@@ -6,7 +6,8 @@
 @section('breadcrumb')
 <ol class="breadcrumb border-0 m-0">
     <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-    <li class="breadcrumb-item active">{{$module_title}}</li>
+    <li class="breadcrumb-item"><a href="{{ url()->previous() }}">Iventory</a></li>
+    <li class="breadcrumb-item active">{{ $type }}</li>
 </ol>
 @endsection
 @section('content')
