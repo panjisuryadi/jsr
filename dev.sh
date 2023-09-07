8b0b2f5617d7feb4cf9109cca8e2f08844fc1753
n0izestr3am <n0izestr3am@gmail.com>
2023-09-07 15:50:10 +0700

    tes |  2023-09-07-15:50:10

---
+++ dev.sh
@@ -0,0 +1,13 @@
+git config --global user.email "n0izestr3am@gmail.com"
+git config --global user.name "n0izestr3am"
+#
+git add .
+
+
+echo 'Judul:'
+read commitMessage
+
+git commit -a -m "$commitMessage |  `date +%F-%T`"
+#
+# Push changes to remote repository
+git push origin dev
