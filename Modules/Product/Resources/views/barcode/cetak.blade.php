c62bcb0f6a74d58d72057e188f38bd3f5843271b
n0izestr3am <n0izestr3am@gmail.com>
2023-09-13 14:50:30 +0700

    |  2023-09-13-14:50:30

--- Modules/Product/Resources/views/barcode/cetak.blade.php
+++ Modules/Product/Resources/views/barcode/cetak.blade.php
@@ -6,25 +6,71 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Barcodes</title>
+
+    <link rel="stylesheet" href="{{ public_path('css/backend.css') }}">
     <link rel="stylesheet" href="{{ public_path('b3/bootstrap.min.css') }}">
+  
+<style>
+.borderless td, .borderless th {
+    border: none;
+}
+div#kanan {
+  transform: rotate(180deg);
+}
+</style>
 </head>
 <body>
 <div class="container">
-    <div class="row">
 
-            <div class="justify-center text-center col-xs-3" style="border: 1px solid #dddddd;border-style: dashed;">
-                <p style="font-size: 15px;color: #000;margin-top: 15px;margin-bottom: 5px;">
-                    {{ $name }}
-                </p>
-                <div>
-            {!! \Milon\Barcode\Facades\DNS2DFacade::getBarCodeSVG($product->product_code,'QRCODE', 9, 9) !!}
 
-                </div>
-                <p class="justify-center text-center" style="font-size: 15px;color: #000;font-weight: bold;">
-                   {{$product->product_code}}</p>
+<table  style="border: 1px solid #dddddd;border-style: dashed;" class="table borderless">
+
+      <tr style="border:none !important;">
+        <td style="border:none !important;text-align: left;">
+
+         <div class="justify-center text-center">
+       
+      <div class="text-center justify-center items-center py-5">
+           
+  {!! \Milon\Barcode\Facades\DNS2DFacade::getBarCodeSVG($product->product_code,'QRCODE', 6, 6) !!}
+
+
+
+       </div>
+          
+           
+                <div class="justify-center text-center" style="font-size: 24px;color: #000;font-weight: bold;">
+                   {{$product->product_code}}</div>
             </div>
- 
-    </div>
+
+
+
+
+        </td>
+        <td style="border:none !important;">
+
+         <div id="kanan" style="transform: rotate(180deg);" class="justify-right text-right">
+       
+       <div class="text-center justify-center items-center py-5">
+           
+  {!! \Milon\Barcode\Facades\DNS2DFacade::getBarCodeSVG($product->product_code,'QRCODE', 6, 6) !!}
+
+
+
+       </div>
+          
+           
+                <div class="justify-center text-center" style="transform: rotate(180deg); font-size: 24px;color: #000;font-weight: bold;">
+                   {{$product->product_code}}</div>
+            </div>
+
+
+    </td>
+      
+      </tr>
+    
+  </table>
+
 </div>
 </body>
 </html>
