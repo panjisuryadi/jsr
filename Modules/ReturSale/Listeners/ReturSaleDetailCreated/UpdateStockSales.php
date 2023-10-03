cd0b3d84c2d5e9fbbd3fb836d57ebb09784c927e
nazzal ramzan <nazzalra@gmail.com>
2023-10-03 17:38:41 +0700

    reports - piutang sales (retur sales)

--- Modules/ReturSale/Listeners/ReturSaleDetailCreated/UpdateStockSales.php
+++ Modules/ReturSale/Listeners/ReturSaleDetailCreated/UpdateStockSales.php
@@ -5,6 +5,7 @@ namespace Modules\ReturSale\Listeners\ReturSaleDetailCreated;
 use Modules\ReturSale\Events\ReturSaleDetailCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Modules\Reports\Models\PiutangSalesReport;
 use Modules\Stok\Models\StockSales;
 
 class UpdateStockSales implements ShouldQueue
@@ -27,6 +28,8 @@ class UpdateStockSales implements ShouldQueue
      */
     public function handle(ReturSaleDetailCreated $event)
     {
+        $retur_sale = $event->retur_sale;
+        $retur_sale_detail = $event->retur_sale_detail;
         $stock_sales = StockSales::where([
             'sales_id' => $event->retur_sale->sales_id,
             'karat_id' => $event->retur_sale_detail->karat_id
@@ -35,6 +38,15 @@ class UpdateStockSales implements ShouldQueue
         $existingWeight = $stock_sales->weight;
         $stock_sales->update([
             'weight' => $existingWeight - $event->retur_sale_detail->weight
+        ]);
+
+        PiutangSalesReport::create([
+            'date' => $retur_sale->date,
+            'karat_id' => $retur_sale_detail->karat_id,
+            'sales_id' => $retur_sale->sales_id,
+            'description' => 'Retur Sales dari ' . $retur_sale->sales->name,
+            'weight_out' => $retur_sale_detail->weight,
+            'remaining_weight' => $stock_sales->weight
         ]);
     }
 }
