968e06043b4c93f6db1ceed39445e4331e98ac2b
nazzal ramzan <nazzalra@gmail.com>
2023-10-17 13:14:40 +0700

    data sales - fix bug on show page

--- Modules/DataSale/Resources/views/datasales/show.blade.php
+++ Modules/DataSale/Resources/views/datasales/show.blade.php
@@ -67,7 +67,7 @@
                                 <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
                                     <div class="px-3 pt-8 pb-10 text-center relative z-10">
                                         <h4 class="text-sm uppercase text-gray-500 leading-tight">Insentif</h4>
-                                        <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">Rp. {{ rupiah($detail->insentif->nominal??0) }}</h3>
+                                        <h3 class="text-3xl text-gray-700 font-semibold leading-tight my-3">Rp. {{ rupiah($detail->insentif->count()?$detail->insentif->first()->nominal:0) }}</h3>
                                     </div>
                                     <div class="absolute bottom-0 inset-x-0">
                                         <canvas id="chart1" height="70"></canvas>
