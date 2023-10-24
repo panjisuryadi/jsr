4c922d49a8cda6b78fa3b0d600183699f806d4db
nazzal ramzan <nazzalra@gmail.com>
2023-10-24 17:28:36 +0700

    karat - update karat / model karat master data form

--- app/Http/Livewire/Karat/Create.php
+++ app/Http/Livewire/Karat/Create.php
@@ -7,6 +7,8 @@ use Livewire\Component;
 use App\Models\User;
 use Modules\Karat\Models\Karat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 use Modules\Karat\Http\Controllers\KaratsController;
 
 class Create extends Component
@@ -15,11 +17,13 @@ class Create extends Component
 
     public $parent_karat_id = '';
 
-    public $kategori_karat = '';
+    public $name = '';
+
+    public $model = '';
 
     public $kode = '';
-    public $kadar = '';
 
+    public $type = '';
     public $isParentSelected = false;
 
     public function mount(){
@@ -32,13 +36,18 @@ class Create extends Component
     public function render(){
         return view('livewire.karat.create');
     }
-    public function setCode(){
+    public function parentSelected(){
         if($this->parent_karat_id != ''){
-            $this->kode = Karat::find($this->parent_karat_id)->kode;
+            $karat = Karat::find($this->parent_karat_id);
+            $this->kode = $karat->kode;
+            $this->type = $karat->type;
             $this->isParentSelected = true;
+            $this->name = '';
         }else{
             $this->isParentSelected = false;
             $this->kode = '';
+            $this->type = '';
+            $this->model = '';
         }
         $this->dispatchBrowserEvent('parentChange', ['isParentSelected' => $this->isParentSelected]);
     }
@@ -47,9 +56,34 @@ class Create extends Component
     {
         $rules = [
             'kode' => 'required',
-            'kadar' => 'required',
             'type' => 'required',
-            'kategori_karat' => 'requiredIf:isParentSelected,true'
+            'model' => [
+                Rule::requiredIf(function () {
+                    return $this->parent_karat_id !== '';
+                }),
+                function ($attribute, $value, $fail) {
+                    if($this->parent_karat_id != ''){
+                        $exist = Karat::where('parent_id',$this->parent_karat_id)->where(DB::raw('lower(name)'), strtolower($value))->exists();
+                        if ($exist) {
+                            $fail('Model sudah ada untuk karat yang sama');
+                        }
+                    }else{
+                        $exist = Karat::where(DB::raw('lower(name)'), strtolower($value))->exists();
+                        if ($exist) {
+                            $fail('Nama Karat sudah digunakan');
+                        }
+                    }
+                },
+            ],
+            'name' => [
+                "requiredIf:parent_karat_id,",
+                function ($attribute, $value, $fail) {
+                    $exist = Karat::where(DB::raw('lower(name)'), strtolower($value))->exists();
+                    if ($exist) {
+                        $fail('Nama Karat sudah digunakan');
+                    }
+                },
+            ]
         ];
         return $rules;
     }
@@ -59,9 +93,9 @@ class Create extends Component
 
         $data = [
             'parent_id' => $this->parent_karat_id?$this->parent_karat_id:null,
-            'name' => $this->kadar,
+            'name' => $this->parent_karat_id?$this->model:$this->name,
             'type' => $this->type,
-            'kode' => $this->isParentSelected?$this->kategori_karat:$this->kode,
+            'kode' => $this->kode,
         ];
 
 
--- resources/views/livewire/karat/create.blade.php
+++ resources/views/livewire/karat/create.blade.php
@@ -6,17 +6,17 @@
         <div class="form-group">
             <?php
             $field_name = 'parent_karat_id';
-            $field_lable = __('Karat Induk');
+            $field_lable = __('Karat');
             $field_placeholder = Label_case($field_lable);
             $invalid = $errors->has($field_name) ? ' is-invalid' : '';
             ?>
             <label class="text-gray-700 mb-0" for="{{ $field_name }}">
                 {{ $field_lable }}</label>
-            <select class="form-control" name="{{ $field_name }}" wire:change="setCode" wire:model="{{ $field_name }}">
-                <option value="" selected>Select Karat</option>
+            <select class="form-control" name="{{ $field_name }}" wire:change="parentSelected" wire:model="{{ $field_name }}">
+                <option value="" selected>Pilih Karat</option>
                 @foreach($dataKarat as $karat)
                 <option value="{{$karat->id}}">
-                    {{$karat->name}} | {{$karat->kode}}
+                    {{$karat->name}}
                 </option>
                 @endforeach
             </select>
@@ -24,8 +24,29 @@
 
         <div class="form-group">
             <?php
+            $field_name = 'name';
+            $field_lable = __('Nama Karat');
+            $field_placeholder = Label_case($field_lable);
+            $invalid = $errors->has($field_name) ? ' is-invalid' : '';
+            ?>
+            <label class="text-gray-700 mb-0" for="{{ $field_name }}">
+                {{ $field_lable }}
+                @if ($parent_karat_id == '')
+                <span class="text-danger">*</span>
+                @endif
+            </label>
+            <input class="form-control" type="text" name="{{ $field_name }}" id="{{ $field_name }}" placeholder="{{ $field_placeholder }}" wire:model="{{ $field_name }}" wire:ignore>
+            @if ($errors->has($field_name))
+            <span class="invalid feedback" role="alert">
+                <small class="text-danger">{{ $errors->first($field_name) }}.</small class="text-danger">
+            </span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <?php
             $field_name = 'kode';
-            $field_lable = label_case($field_name);
+            $field_lable = label_case('Kode / Kadar');
             $field_placeholder = $field_lable;
             $invalid = $errors->has($field_name) ? ' is-invalid' : '';
             $required = "required";
@@ -44,13 +65,17 @@
 
         <div class="form-group">
             <?php
-            $field_name = 'kategori_karat';
+            $field_name = 'model';
             $field_lable = label_case($field_name);
             $field_placeholder = $field_lable;
             $invalid = $errors->has($field_name) ? ' is-invalid' : '';
             $required = "required";
             ?>
-            <label class="text-gray-700 mb-0" for="{{ $field_name }}">{{ $field_lable }}<span class="text-danger">*</span></label>
+            <label class="text-gray-700 mb-0" for="{{ $field_name }}">{{ $field_lable }}
+                @if ($parent_karat_id != '')
+                <span class="text-danger">*</span>
+                @endif
+            </label>
             <input class="form-control" type="text" name="{{ $field_name }}" id="{{ $field_name }}" placeholder="{{ $field_placeholder }}" wire:model="{{ $field_name }}" readonly wire:ignore>
             <span class="invalid feedback" role="alert">
                 <span class="text-danger error-text {{ $field_name }}_err"></span>
@@ -62,42 +87,25 @@
             @endif
         </div>
 
-
         <div class="form-group">
-            <?php
-            $field_name = 'kadar';
-            $field_lable = label_case('Kadar');
+           <?php
+            $field_name = 'type';
+            $field_lable = label_case('Tipe');
             $field_placeholder = $field_lable;
             $invalid = $errors->has($field_name) ? ' is-invalid' : '';
             $required = "required";
             ?>
             <label class="text-gray-700 mb-0" for="{{ $field_name }}">{{ $field_lable }}<span class="text-danger">*</span></label>
-            <input class="form-control" type="text" name="{{ $field_name }}" id="{{ $field_name }}" placeholder="{{ $field_placeholder }}" wire:model="{{ $field_name }}">
-            <span class="invalid feedback" role="alert">
-                <span class="text-danger error-text {{ $field_name }}_err"></span>
-            </span>
+            <select wire:model="{{ $field_name }}" class="form-control" name="{{ $field_name }}" id="{{ $field_name }}" wire:ignore>
+                <option value="" selected disabled>Pilih Tipe</option>
+                <option value="LM">Logam Mulia</option>
+                <option value="GOLD">Emas</option>
+            </select>
             @if ($errors->has($field_name))
             <span class="invalid feedback" role="alert">
                 <small class="text-danger">{{ $errors->first($field_name) }}.</small class="text-danger">
             </span>
             @endif
-        </div>
-
-
-        <div class="form-group">
-           <?php
-            $field_name = 'type';
-            $field_lable = label_case('Type');
-            $field_placeholder = $field_lable;
-            $invalid = $errors->has($field_name) ? ' is-invalid' : '';
-            $required = "required";
-            ?>
-            <label class="text-gray-700 mb-0" for="{{ $field_name }}">{{ $field_lable }}<span class="text-danger">*</span></label>
-            <select wire:model="{{ $field_name }}" class="form-control" name="{{ $field_name }}" id="{{ $field_name }}" required>
-                <option value="" selected disabled>Select Type</option>
-                <option value="LM">Logam Mulia</option>
-                <option value="GOLD">Emas</option>
-            </select>
         </div>
 
 
@@ -127,14 +135,20 @@
 @push('page_scripts')
 <script type="text/javascript">
     let kode = document.getElementById('kode');
-    let kategori_karat = document.getElementById('kategori_karat');
+    let type = document.getElementById('type');
+    let name = document.getElementById('name');
+    let model = document.getElementById('model');
     window.addEventListener('parentChange', (e) => {
         if (e.detail.isParentSelected) {
             kode.setAttribute('readonly', true);
-            kategori_karat.removeAttribute('readonly');
+            type.setAttribute('readonly',true);
+            name.setAttribute('readonly',true);
+            model.removeAttribute('readonly');
         } else {
             kode.removeAttribute('readonly');
-            kategori_karat.setAttribute('readonly',true);
+            type.removeAttribute('readonly');
+            name.removeAttribute('readonly');
+            model.setAttribute('readonly',true);
         }
     });
 </script>
