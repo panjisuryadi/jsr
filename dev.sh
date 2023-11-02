67c8095448ab263e334a23aaa7d9c05f23383d61
n0izestr3am <n0izestr3am@gmail.com>
2023-11-02 16:47:18 +0700

    |  2023-11-02-16:47:18

--- dev.sh
+++ dev.sh
@@ -1,14 +1,6 @@
 git config --global user.email "n0izestr3am@gmail.com"
 git config --global user.name "n0izestr3am"
 #
-git stash
-
-git pull origin dev
-
-git stash pop
-
-sudo chown ipunk:ipunk -R /var/www/html/hokkie_jsr
-
 
 git add .
 
