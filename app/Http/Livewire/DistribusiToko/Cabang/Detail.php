13a9cb4fa4bf2f7f4c9094ff42947d5b00f76daa
nazzal ramzan <nazzalra@gmail.com>
2023-11-03 17:05:07 +0700

    distribusi toko [cabang] - init summary modal

---
+++ app/Http/Livewire/DistribusiToko/Cabang/Detail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\DistribusiToko\Cabang;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Livewire\Component;
+use Modules\Product\Entities\Product;
+
+class Detail extends Component
+{
+    public $dist_toko;
+
+    public $selectedItems = [];
+
+
+    public function render()
+    {
+        return view("livewire.distribusi-toko.cabang.detail");
+    }
+
+    public function mount()
+    {
+    }
+
+    public function rules()
+    {
+        $rules = [];
+
+
+        return $rules;
+    }
+
+    public function proses()
+    {
+        if (count($this->selectedItems) == 0) {
+            $this->dispatchBrowserEvent('items:not-selected', [
+                'message' => 'Barang belum dipilih'
+            ]);
+        } else {
+            $this->dispatchBrowserEvent('summary:modal');
+        }
+    }
+}
