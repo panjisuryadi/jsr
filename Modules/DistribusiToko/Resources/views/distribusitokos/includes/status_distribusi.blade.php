dfc9fd5fef3934b7ea966401a0cac0a2a116e561
n0izestr3am <n0izestr3am@gmail.com>
2023-11-07 14:00:40 +0700

    |  2023-11-07-14:00:40

--- Modules/DistribusiToko/Resources/views/distribusitokos/includes/status_distribusi.blade.php
+++ Modules/DistribusiToko/Resources/views/distribusitokos/includes/status_distribusi.blade.php
@@ -6,6 +6,9 @@
 @elseif($data->current_status->id == 3)
 <button class="w-full btn uppercase btn-outline-danger px  btn-sm">Retur</button>
 
+@elseif($data->current_status->id == 4)
+<button class="w-full btn uppercase btn-outline-info px btn-sm">Completed</button>
+
 @elseif($data->current_status->id == 1)
 <button class="w-full btn uppercase btn-success px btn-sm">Draft</button>
 @endif
