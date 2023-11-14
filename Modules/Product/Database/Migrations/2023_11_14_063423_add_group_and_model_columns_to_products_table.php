253b538e72527bc9597464b07954153cdde45193
nazzal ramzan <nazzalra@gmail.com>
2023-11-14 09:41:03 +0700

    fix bug

--- Modules/Product/Database/Migrations/2023_11_14_063423_add_group_and_model_columns_to_products_table.php
+++ Modules/Product/Database/Migrations/2023_11_14_063423_add_group_and_model_columns_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddGroupAndModelToProductsTable extends Migration
+class AddGroupAndModelColumnsToProductsTable extends Migration
 {
     /**
      * Run the migrations.
