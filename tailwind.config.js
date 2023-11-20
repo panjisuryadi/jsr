3ccd8ca30bb1707229b8e49b7d29f4c46c6713cb
nazzal ramzan <nazzalra@gmail.com>
2023-11-20 10:08:40 +0700

    configure livewire datatable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,41 @@
 module.exports = {
-    content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-
+    mode: "jit",
+    purge: [
+        "./vendor/rappasoft/laravel-livewire-tables/resources/views/**/*.blade.php",
     ],
-     theme: {
-    container: {
-      center: true,
-      screens: {
-        '2xl': {'max': '1536px'},
-        'xl': {'max': '1280px'},
-        'lg': {'max': '1023px'},
-        'md': {'max': '767px'},
-        'sm': {'max': '639px'},
-      },
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                "2xl": { max: "1536px" },
+                xl: { max: "1280px" },
+                lg: { max: "1023px" },
+                md: { max: "767px" },
+                sm: { max: "639px" },
+            },
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
+        extend: {
+            colors: {},
+            fontSize: {
+                vl: "76px",
+            },
+            fontFamily: {
+                display: ["Poppins", "Inter", "system-ui", "sans-serif"],
+                body: ["Poppins", "Inter", "system-ui", "sans-serif"],
+            },
+        },
     },
-    extend: {
-      colors: {},
-      fontSize: {
-        'vl': '76px',
-      },
-      fontFamily: {
-      display: ['Poppins','Inter', 'system-ui', 'sans-serif'],
-      body: ['Poppins', 'Inter', 'system-ui', 'sans-serif'],
-    }
-    },
-  },
     plugins: [],
-  }
+};
