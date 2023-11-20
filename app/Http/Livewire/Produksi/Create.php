646fa535cedf4d2892e0cba3b78e33b2e92102c7
dede.herdiana <dede.herdiana@docotel.com>
2023-11-20 16:08:47 +0700

    validasi stok ke goodsreceipt items

--- app/Http/Livewire/Produksi/Create.php
+++ app/Http/Livewire/Produksi/Create.php
@@ -276,13 +276,15 @@ class Create extends Component
               * bandingkan stok
               */
             if($this->kategoriproduk_id == $this->id_kategoriproduk_berlian && !empty($this->inputs[0]['karatberlians'])) {
-                $sisa_stok_berlian = GoodsReceipt::where('kategoriproduk_id', $this->id_kategoriproduk_berlian)->sum('total_karat');
+                // $sisa_stok_berlian = GoodsReceipt::where('kategoriproduk_id', $this->id_kategoriproduk_berlian)->sum('total_karat');
+                $sisa_stok_berlian = 0;
                 $total_karat_dipinta = 0;
                 foreach ($this->inputs as $key => $value) {
                     $rules['inputs.' . $key . '.id_items'] = 'required';
                     $rules['inputs.' . $key . '.karatberlians'] = 'required|gt:0';
                     $rules['inputs.' . $key . '.qty'] = 'required|gt:0';
-
+                    $sisa_stok_berlian = GoodsReceiptItem::where('id', $value['id_items'])->first();
+                    $sisa_stok_berlian = $sisa_stok_berlian->karatberlians - $sisa_stok_berlian->karatberlians_terpakai;
                     $qty = !empty($value['qty']) ? $value['qty'] : 0;
                     $karat = !empty($value['karatberlians']) ? $value['karatberlians'] : 0;
                     if(filter_var($karat, FILTER_VALIDATE_FLOAT)) {
@@ -483,7 +485,6 @@ class Create extends Component
             if(!empty($file)) {
                 Storage::disk('public')->delete($file);
             }
-            dd($th->getMessage());
             return $th->getMessage();
         }
         DB::commit();
