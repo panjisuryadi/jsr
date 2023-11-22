04916d0654b6f21f718516e2047df1e4b6e5c4af
n0izestr3am <n0izestr3am@gmail.com>
2023-11-22 12:58:39 +0700

    |  2023-11-22-12:58:39

--- Modules/Product/Resources/views/products/partials/actions.blade.php
+++ Modules/Product/Resources/views/products/partials/actions.blade.php
@@ -1,6 +1,6 @@
-@can('approve_distribusi')
+@can('show_products')
 <a id="Show" href="{{ route('products.show', $data->id) }}" class="btn btn-outline-danger btn-sm">
-    <i class="bi bi-eye"></i>Approve
+    <i class="bi bi-eye"></i>&nbsp;Detail
 </a>
 @endcan
 
