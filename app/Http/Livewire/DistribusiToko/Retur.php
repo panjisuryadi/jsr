7021b010f22d727d1a161a07153519486c3c3b02
nazzal ramzan <nazzalra@gmail.com>
2023-11-24 08:55:06 +0700

    distribusi toko emas - update approval and retur

--- app/Http/Livewire/DistribusiToko/Retur.php
+++ app/Http/Livewire/DistribusiToko/Retur.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Livewire\Component;
 use Modules\Product\Entities\Product;
+use Modules\Product\Models\ProductStatus;
 use Modules\Stok\Models\StockOffice;
 
 class Retur extends Component
@@ -41,23 +42,11 @@ class Retur extends Component
     public function handleIsApproved($is_approved){
         if($is_approved){
             foreach($this->dist_toko->items()->returned()->get() as $item){
-                $stock_office = StockOffice::where('karat_id', $item->karat_id)->first();
-                if(is_null($stock_office)){
-                    $stock_office = StockOffice::create(['karat_id'=> $item->karat_id]);
-                }
-                $item->stock_office()->attach($stock_office->id,[
-                    'karat_id'=>$item->karat_id,
-                    'in' => true,
-                    'berat_real' =>$item->gold_weight,
-                    'berat_kotor' => $item->gold_weight
-                ]);
-                $berat_real = $stock_office->history->sum('berat_real');
-                $berat_kotor = $stock_office->history->sum('berat_kotor');
-                $stock_office->update(['berat_real'=> $berat_real, 'berat_kotor'=>$berat_kotor]);
+                $item->product->updateTracking(ProductStatus::PENDING_OFFICE);
             }
             $this->dist_toko->setAsCompleted();
             toast('Berhasil Menambah Stok Gudang','success');
-            return redirect(route('distribusitoko.index'));
+            return redirect(route('distribusitoko.emas'));
         }
     }
 
