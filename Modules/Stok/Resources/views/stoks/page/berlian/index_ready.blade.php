ca9205fd2d3bd4112b4d189a4727f1e19239fe08
dede.herdiana <dede.herdiana@docotel.com>
2023-12-07 11:39:42 +0700

    stock ready berlian

---
+++ Modules/Stok/Resources/views/stoks/page/berlian/index_ready.blade.php
@@ -0,0 +1,235 @@
+@extends('layouts.app')
+@section('title', $module_title)
+@section('third_party_stylesheets')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+@endsection
+@section('breadcrumb')
+<ol class="breadcrumb border-0 m-0">
+    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+    <li class="breadcrumb-item active">{{$module_title}} {{ $module_action }}</li>
+</ol>
+@endsection
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="flex justify-between py-1 border-bottom">
+                        <div>
+                            <p class="uppercase text-lg text-gray-600 font-semibold">
+                                Stok 
+                                <span class="text-yellow-500 uppercase">{{$module_action}}</span>
+                             </p>
+                        </div>
+                        <div id="buttons">
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-2 gap-2 mb-3 px-3 py-2 mt-2 rounded-md border border-gray-300">
+                        @if (count($datakarat))
+                            {{-- <div class="py-2 px-2 font-semibold flex flex-col justify-center">
+                                <p class="text-gray-400 text-lg">Karat : <span class="text-gray-800" id="karat"></span></p>
+                                <p class="text-gray-400 text-lg">Jumlah Stok : <span class="text-gray-800" id="sisa-stok"></span></p>
+                            </div> --}}
+                            <div>
+                            @if (!auth()->user()->isUserCabang())
+                                <div class="form-group">
+                                    <label for="cabang_filter" class="font-bold mb-0">Cabang</label>
+                                    <select name="cabang_filter" id="cabang_filter" class="form-control">
+                                        <option value="" selected>Pilih Cabang</option>
+                                        @foreach ($dataCabang as $cabang )
+                                        <option value="{{$cabang->id}}">{{ $cabang->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            @endif
+                            <div class="form-group">
+                                <label for="karat_filter" class="mb-0 font-semibold">Karat</label>
+                                <select name="karat_filter" id="karat_filter" class="form-control">
+                                    <option value="" selected>Pilih Karat</option>
+                                    @foreach ($datakarat as $karat )
+                                        <option value="{{$karat->id}}">{{ $karat->name }} | {{$karat->kode}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        @endif
+                    </div>
+
+                </div>
+                    
+                <div class="table-responsive mt-1">
+
+                    <table id="datatable" style="width: 100%" class="table table-bordered table-hover table-responsive-sm">
+                        <thead>
+                            <tr>
+                            <th style="width: 6%!important;">No</th>
+                            <th class="text-left">{{ Label_Case('Image') }}</th>
+                            <th class="text-left">{{ Label_Case('Product') }}</th>
+                            <th class="text-left">{{ Label_Case('Cabang') }}</th>
+                            <th class="text-left">{{ Label_Case('Karat') }}</th>
+                            <th class="text-left">{{ Label_Case('Berat') }}</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    @include('product::products.modal.process')
+</div>
+@endsection
+<x-library.datatable />
+@push('page_scripts')
+   <script type="text/javascript">
+        var berlian_kategori = "{{ json_encode($berlian_kategori) }}";
+        $(function(){
+            datatable()
+            getStockInfo()
+        })
+        function datatable(){
+            $('#datatable').DataTable({
+                destroy: true,
+            }).destroy();
+            $('#datatable').DataTable({
+               processing: true,
+               serverSide: true,
+               autoWidth: true,
+               responsive: true,
+               lengthChange: true,
+                searching: true,
+               "oLanguage": {
+                "sSearch": "<i class='bi bi-search'></i> {{ __("labels.table.search") }} : ",
+                "sLengthMenu": "_MENU_ &nbsp;&nbsp;Data Per {{ __("labels.table.page") }} ",
+                "sInfo": "{{ __("labels.table.showing") }} _START_ s/d _END_ {{ __("labels.table.from") }} <b>_TOTAL_ data</b>",
+                "sInfoFiltered": "(filter {{ __("labels.table.from") }} _MAX_ total data)",
+                "sZeroRecords": "{{ __("labels.table.not_found") }}",
+                "sEmptyTable": "{{ __("labels.table.empty") }}",
+                "sLoadingRecords": "Harap Tunggu...",
+                "oPaginate": {
+                    "sPrevious": "{{ __("labels.table.prev") }}",
+                    "sNext": "{{ __("labels.table.next") }}"
+                }
+                },
+                "aaSorting": [[ 0, "desc" ]],
+                "columnDefs": [
+                    {
+                        "targets": 'no-sort',
+                        "orderable": false,
+                    }
+                ],
+                "sPaginationType": "simple_numbers",
+                ajax: {
+                    url : '{{ route("$module_name.index_data_ready") }}?karat='+$('#karat_filter').val()+'&cabang='+$('#cabang_filter').val(),
+                    data : {
+                        catagories : berlian_kategori
+                    },
+                    dataType:'json',
+
+                },
+                dom: 'Blfrtip',
+                buttons: [
+                    'excel',
+                    'pdf',
+                    'print'
+                ],
+                  columns: [{
+                        "data": 'id',
+                        "sortable": false,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+    
+                    {data: 'image', name: 'image'},
+                    {data: 'product', name: 'product'},
+                    {data: 'cabang', name: 'cabang'},
+                    {data: 'karat', name: 'karat'},
+                    {data: 'weight', name: 'weight'},
+                ]
+            })
+            .buttons().remove()
+            .container()
+            .appendTo("#buttons");
+        }
+        $('#karat_filter').change(function(){
+            datatable();
+            getStockInfo();
+        });
+
+        $('#cabang_filter').change(function(){
+            datatable()
+            getStockInfo()
+        });
+
+        
+       
+
+        function getStockInfo(){
+            $.ajax({
+                type: "get",
+                url: '{{ route("$module_name.get_stock_ready") }}?karat='+$('#karat_filter').val()+'&cabang='+$('#cabang_filter').val(),
+                dataType:'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    'Accept' : 'application/json'
+                },
+                success: function(data){
+                    $('#karat').text(data.karat)
+                    $('#sisa-stok').text(data.sisa_stok + ' gr')
+                }
+            })
+        }
+
+
+        function process(data){
+            $('#product-process-modal').modal('show')
+            $('#product-process-modal #data_id').val(data.id)
+        }
+
+        $('#form-product-process').submit(function(){
+            $.ajax({
+                type: "POST",
+                url: '{{route("products.update_status")}}',
+                data: $('#form-product-process').serialize(),
+                dataType:'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    'Accept' : 'application/json'
+                },
+                success: function(data){
+                    if(data.status == 'success'){
+                        toastr.success(data.message)
+                        setTimeout(function(){ 
+                           window.location.reload()
+                            $('#product-process-modal').modal('hide')
+                        }, 1000);
+                    }else{
+                        toastr.error(data.message)
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) { // if error occured
+                        data = JSON.parse(jqXHR.responseText);
+                        if(data.message == 'The given data was invalid.'){
+                            err = data.errors;
+                            $.each(err, function(key, val) {
+                                $("."+key+"_field .fv-plugins-message-container").text(val);                   
+                                $("."+key+"_field .fv-plugins-message-container").show();
+                            });
+
+                            toastr.error(data.message);
+                        }
+                        else{
+                            toastr.error("Error occured. "+jqXHR.status+" "+ textStatus +" "+" please try again");
+                        }
+                    }
+            })
+            return false;
+        })
+
+
+
+    </script>
+
+@endpush
