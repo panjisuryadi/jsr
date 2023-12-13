779c221c55999a890f7d1117b4af0df5c8c2d9e7
n0izestr3am <n0izestr3am@gmail.com>
2023-12-13 09:34:51 +0700

    kepala toko |  2023-12-13-09:34:51

--- resources/views/partial/pages/kepala_toko.blade.php
+++ resources/views/partial/pages/kepala_toko.blade.php
@@ -87,6 +87,7 @@
   </div>
 </div>
 
+{{-- nav  tab --}}
 
     <ul class="nav nav-tabs py-1" role="tablist">
 
@@ -109,7 +110,7 @@
         @endcan
 
         <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#Penjualan">Penjualan</a>
+            <a class="nav-link {{ $paging == 'sales' ? 'active' : '' }}" data-toggle="tab" href="#Penjualan">Penjualan</a>
         </li>
 
          <li class="nav-item">
@@ -120,410 +121,41 @@
             <a class="nav-link" data-toggle="tab" href="#StockReady">Stok Ready</a>
         </li>
     </ul>
+{{-- end nav  tab --}}
+
+
+{{-- start content tab --}}
+
 
     <div class="tab-content py-3 mb-2">
  <div id="Buysback" class="container px-0 tab-pane {{ $paging == 'buysbacks' ? 'active' : '' }}">
-            <div class="pt-3">
-
-
-<table style="width: 100%;" class="table table-striped table-bordered">
-                        <tr>
-                            <th class="text-center">{{ label_case('No') }}</th>
-                            <th>{{ label_case('no_buys_back') }}</th>
-                            <th>{{ label_case('Tanggal') }}</th>
-                            <th>{{ label_case('Cabang') }}</th>
-                            <th>{{ label_case('karat') }}</th>
-                            <th>{{ label_case('berat') }}</th>
-
-                            <th>{{ label_case('Status') }}</th>
-                            <th>{{ label_case('Aksi') }}</th>
-                        </tr>
-                @php
-                  $buysbacks = \Modules\BuysBack\Models\BuysBack::latest()->paginate(5, ['*'], 'buysbacks');
-                @endphp
-                        @forelse($buysbacks as $sale)
-                           {{--  @if($loop->index > 3)
-                               @break
-                            @endif
- --}}
-
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td class="text-blue-400">{{ shortdate($sale->date) }}</td>
-                            <td>{{ $sale->no_buy_back }}</td>
-                            <td>{{ $sale->cabang->name }}</td>
-                            <td>{{ $sale->karat->name }}</td>
-                            <td>{{ $sale->weight }}</td>
-
-
-                            <td>
-
-                         <a href="{{ route('buysback.status', $sale->id) }}"
-                            id="Status"
-                            data-toggle="tooltip"
-                             class="btn {{bpstts($sale->current_status?$sale->current_status->name:'PENDING')}} btn-sm uppercase">
-                               {{$sale->current_status?$sale->current_status->name:'PENDING'}}
-                            </a>
-
-                            </td>
-                            <td>
-
-                        <a href="{{ route('buysback.show', $sale->id) }}"
-                            data-toggle="tooltip"
-                             class="btn btn-sm btn-outline-info uppercase">Detail
-                            </a>
-                            </td>
-                        </tr>
-                        @empty
-                            <tr>
-                                <td colspan="8"> <p class="uppercase">Tidak ada Data</p></td>
-
-                            </tr>
-                        @endforelse
-                <tr>
-                    <td colspan="8">
-                        <div class="float-right">
-                            
-          {{ $buysbacks->links('pagination.custom', ['paginator' => $buysbacks, 'paginationKey' => 'buysbacks']) }}
-
-                        </div>
-                    </td>  
-                </tr>
-
-
-                    </table>
-
-      
-                      
-
-            </div>
+     @include('partial.pages.tab.buysbacks')
         </div>
 
-  <div id="distribusitoko" class="container px-0 tab-pane  
-  {{ $paging == 'distribusitoko' ? 'active' : '' }}">
-            <div class="pt-3">
-           
-        <table style="width: 100%;" class="table table-striped table-bordered">
-            <tr>
-                <th class="text-center">{{ label_case('No') }}</th>
-                <th class="text-center">{{ label_case('Cabang') }}</th>
-                <th class="text-center">{{ label_case('Date') }}</th>
-                <th class="text-center">{{ label_case('Invoice') }}</th>
-                <th class="text-center">{{ label_case('Items') }}</th>
-                <th class="text-center">{{ label_case('Status') }}</th>
-                <th class="text-center">{{ label_case('Pic') }}</th>
-                <th class="text-center">{{ label_case('Aksi') }}</th>
-            </tr>
-            @php
-            $distribusitoko = \Modules\DistribusiToko\Models\DistribusiToko::inprogress()->latest()->paginate(2, ['*'], 'distribusitoko');
-            @endphp
-            @forelse($distribusitoko as $row)
-            {{--     @if($loop->index > 4)
-            @break
-            @endif
-            --}}
-            {{-- {{ $row }} --}}
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $row->cabang->name }}</td>
-                <td>{{ shortdate($row->date) }}</td>
-                <td>{{ $row->no_invoice }}</td>
-                <td>{{ $row->items->count() }}</td>
-                <td>
-                    @if($row->current_status->id == 2)
-                    <button class="w-full btn uppercase btn-outline-warning px  leading-5 btn-sm">In Progress</button>
-                    @endif
-                </td>
-                <td>{{ $row->created_by }}</td>
-                <td class="text-center">
-                    <a  href="{{ route('distribusitoko.detail_distribusi', $row->id) }}" class="btn btn-success px-4 btn-sm w-full">
-                        <i class="bi bi-eye"></i> Approve
-                    </a>
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="8"> <p class="uppercase">Tidak ada Data</p></td>
-            </tr>
-            @endforelse
-            <tr>
-                <td colspan="8">
-                    <div class="float-right">
-                        {{ $distribusitoko->links('pagination.custom', ['paginator' => $distribusitoko, 'paginationKey' => 'distribusitoko']) }}
-                    </div>
-                </td>
-            </tr>
-        </table>
-        </div>
-</div>
+    <div id="distribusitoko" class="container px-0 tab-pane  
+      {{ $paging == 'distribusitoko' ? 'active' : '' }}">
+        @include('partial.pages.tab.distribusitoko')
+    </div>
 
-
-
-  <div id="StockPending" class="container px-0 tab-pane">
-            <div class="pt-3">
-
-                   <table style="width: 100%;" class="table table-striped table-bordered">
-                        <tr>
-                            <th class="text-center">{{ label_case('No') }}</th>
-                            <th>{{ label_case('Sales') }}</th>
-                            <th>{{ label_case('karat') }}</th>
-                            <th>{{ label_case('Berat') }}</th>
-                            <th>{{ label_case('Aksi') }}</th>
-                        </tr>
-                      @php
-                   $StockPending = \Modules\Stok\Models\StockPending::latest()->paginate(5);
-                   @endphp
-
-                    @forelse($StockPending as $row)
-                            <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $row->karat->kode }} | {{ $row->karat->name }}</td>
-                            <td>{{ $row->cabang->name }}</td>
-                            <td>{{ $row->weight ?? ' - ' }}</td>
-                            <td>
-
-                                    <a href="{{ route("sales.show",$row->id) }}"
-                                     class="btn btn-outline-success btn-sm">
-                                        <i class="bi bi-eye"></i>&nbsp;@lang('Detail')
-                                    </a>
-                                @can('show_sales')
-                                @endcan
-
-
-                            </td>
-                        </tr>
-                        @empty
-                        <p>Tidak ada Data</p>
-                        @endforelse
-
-                    </table>
-                      @if($StockPending->links()->paginator->hasPages())
-                    <div class="has-text-centered">
-                        {{ $StockPending->links() }}
-                    </div>
-                @endif
-
-            </div>
+  <div id="StockPending" class="container px-0 tab-pane {{ $paging == 'stockpending' ? 'active' : '' }}">
+      @include('partial.pages.tab.stockpending')
         </div>
 
-
-
-
-
-
-  <div id="Penjualan" class="container px-0 tab-pane">
-            <div class="pt-3">
-                   <table style="width: 100%;" class="table table-striped table-bordered">
-                        <tr>
-                            <th class="text-center">{{ label_case('No') }}</th>
-                            <th>{{ label_case('invoice_no') }}</th>
-                            <th>{{ label_case('Cabang') }}</th>
-                            <th>{{ label_case('Customer') }}</th>
-                            <th>{{ label_case('Total') }}</th>
-                            <th>{{ label_case('Status') }}</th>
-                            <th>{{ label_case('Aksi') }}</th>
-                        </tr>
-                   @php
-                   $sales = \Modules\Sale\Entities\Sale::akses()->latest()->paginate(5);
-                   @endphp
-
-                    @forelse($sales as $row)
-
-                          <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $row->reference }}</td>
-                            <td>{{ $row->cabang->name }}</td>
-                            <td>{{ $row->customer_name}}</td>
-                            <td>
-
-                                {{-- Rp .<strong>{{ rupiah($row->total_amount)}}</strong> --}}
-                                <div class="items-center justify-items-center text-center">
-                                @if ($row->tipe_bayar == 'cicilan')
-                                    <span class="text-center text-dark">
-                                       {{ rupiah($row->total_amount) }}
-                                       <div>  {{ shortdate($row->tgl_jatuh_tempo) }} </div>
-                                    </span>
-                                @elseif ($row->status == 'total')
-                                    <span class="badge badge-primary">
-                                        {{ $row->status }}
-                                    </span>
-                                @else
-                                    <span class="text-center text-dark">
-                                        {{ rupiah($row->total_amount) }}
-                                    </span>
-                                @endif
-                                </div>
-
-
-                            </td>
-                            <td>
-                             <div class="items-center justify-items-center text-center">
-                                    @if ($row->tipe_bayar == 'cicilan')
-                                    <a
-                                    class="btn btn-sm btn-info"
-                                    id="Show"
-                                    href="{{ route('sales.show_cicilan', $row->id) }}">
-                                    &nbsp;@lang('Cicilan')
-                                    </a>
-                                    @elseif ($row->status == 'total')
-                                        <span class="badge badge-primary">
-                                            {{ $row->status }}
-                                        </span>
-                                    @else
-                                        <span class="badge badge-success">
-                                            Tunai
-                                        </span>
-                                    @endif
-                                    </div>
-                            </td>
-                            <td>
-
-                                @can('show_sales')
-                                    <a href="{{ route("sales.show",$row->id) }}"
-                                     class="btn btn-outline-success btn-sm">
-                                        <i class="bi bi-eye"></i> &nbsp;@lang('Detail')
-                                    </a>
-                                @endcan
-                                    <a class="btn btn-outline-warning btn-sm" target="_blank" href="{{ route('sales.invoice', $row->id) }}"> <i class="bi bi-printer"></i>&nbsp;@lang('Print')</a>
-                                  @can('print_sales')
-                                     @endcan
-
-
-                            </td>
-                        </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5"> <p class="uppercase">Tidak ada Data</p></td>
-
-                            </tr>
-                        @endforelse
-
-                    </table>
-                       @if($sales->links()->paginator->hasPages())
-                    <div class="has-text-centered">
-                        {{ $sales->links() }}
-                    </div>
-                    @endif
-
-            </div>
+  <div id="Penjualan" class="container px-0 tab-pane {{ $paging == 'sales' ? 'active' : '' }}">
+           @include('partial.pages.tab.sales')
         </div>
 
-
-
-
-  <div id="BuysBacNota" class="container px-0 tab-pane">
-            <div class="pt-3">
-
-                   <table style="width: 100%;" class="table table-striped table-bordered">
-                        <tr>
-                            <th class="text-center">{{ label_case('No') }}</th>
-                            <th class="text-center">{{ label_case('Date') }}</th>
-                            <th>{{ label_case('invoice_no') }}</th>
-                            <th>{{ label_case('Invoice Series') }}</th>
-                            <th>{{ label_case('Cabang') }}</th>
-                            <th>{{ label_case('Pic') }}</th>
-                         
-                        </tr>
-        @php
-        $buybacknota =\Modules\BuysBack\Models\BuyBackNota::latest()->paginate(5);
-       @endphp
-
-                    @forelse($buybacknota as $row)
-                           <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ shortdate($row->date) }}</td>
-                                <td>{{ $row->invoice }}</td>
-                                <td>{{ $row->invoice_series }}</td>
-                                <td>{{ @$row->cabang->name }}</td>
-                                <td>{{ @$row->pic->name }}</td>
-                          
-                        </tr>
-                        @empty
-                           <tr>
-                                <td colspan="6"> <p class="uppercase">Tidak ada Data</p></td>
-
-                            </tr>
-                        @endforelse
-
-                    </table>
-                     @if($buybacknota->links()->paginator->hasPages())
-                    <div class="has-text-centered">
-                        {{ $buybacknota->links() }}
-                    </div>
-                    @endif
-
-
-            </div>
+  <div id="BuysBacNota" class="container px-0 tab-pane {{ $paging == 'buysbacknota' ? 'active' : '' }}">
+             @include('partial.pages.tab.buysbacknota') 
         </div>
 
-  <div id="StockReady" class="container px-0 tab-pane">
-            <div class="pt-3">
-
-                   <table style="width: 100%;" class="table table-striped table-bordered">
-                        <tr>
-                            <th class="text-center">{{ label_case('No') }}</th>
-                            <th class="text-center">{{ label_case('Date') }}</th>
-                            <th>{{ label_case('Produk') }}</th>
-                            <th>{{ label_case('Karat') }}</th>
-                            <th>{{ label_case('Status') }}</th>
-
-                        </tr>
-
-              @php
-            $product =\Modules\Product\Entities\Product::ready()->latest()->paginate(5);
-            @endphp   
-                    @forelse($product as $row)
-                       {{--  @if($loop->index > 4)
-                             @break
-                         @endif --}}
-                           {{-- {{ $row }} --}}
-
-                           <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ tgl($row->created_at) }}</td>
-                                <td>{{ @$row->product_name }}</td>
-                                <td>{{ @$row->karat->name }}</td>
-                                <td>
-                                    <span class="bg-green-500 px-2 py-1 text-center text-white uppercase rounded-md">  {{ @$row->product_status->name }}</span>
-                                  
-                                </td>
-
-                        </tr>
-                        @empty
-                           <tr>
-                                <td colspan="6"> <p class="uppercase">Tidak ada Data</p></td>
-
-                            </tr>
-                        @endforelse
-
-                    </table>
-                 @if($product->links()->paginator->hasPages())
-                    <div class="has-text-centered">
-                        {{ $product->links() }}
-                    </div>
-                    @endif
-
-
-            </div>
+  <div id="StockReady" class="container px-0 tab-pane {{ $paging == 'stockready' ? 'active' : '' }}">
+          @include('partial.pages.tab.stockready')
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     </div>
 
-
+{{-- end content tab --}}
 
 
 </div>
---
+++ resources/views/partial/pages/tab/stockready.blade.php
@@ -0,0 +1,53 @@
+    <div class="pt-3">
+
+                   <table style="width: 100%;" class="table table-striped table-bordered">
+                        <tr>
+                            <th class="text-center">{{ label_case('No') }}</th>
+                            <th class="text-center">{{ label_case('Date') }}</th>
+                            <th>{{ label_case('Produk') }}</th>
+                            <th>{{ label_case('Karat') }}</th>
+                            <th>{{ label_case('Status') }}</th>
+
+                        </tr>
+
+              @php
+               $stockreadys = \Modules\Product\Entities\Product::ready()->latest()->paginate(2, ['*'], 'stockready');
+              @endphp   
+                    @forelse($stockreadys as $row)
+                       {{--  @if($loop->index > 4)
+                             @break
+                         @endif --}}
+                           {{-- {{ $row }} --}}
+
+                           <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ tgl($row->created_at) }}</td>
+                                <td>{{ @$row->product_name }}</td>
+                                <td>{{ @$row->karat->name }}</td>
+                                <td>
+                                    <span class="bg-green-500 px-2 py-1 text-center text-white uppercase rounded-md">  {{ @$row->product_status->name }}</span>
+                                  
+                                </td>
+
+                        </tr>
+                        @empty
+                           <tr>
+                                <td colspan="6"> <p class="uppercase">Tidak ada Data</p></td>
+
+                            </tr>
+                        @endforelse
+                    @if($stockreadys->links()->paginator->hasPages())
+                       <tr>
+                        <td colspan="8">
+                                <div class="float-right">
+                      {{ $stockreadys->links('pagination.custom', ['paginator' => $stockreadys, 'paginationKey' => 'stockready']) }}
+                                    </div>
+                                </td>  
+                            </tr>
+                       @endif 
+
+
+                    </table>
+                 
+
+            </div>
