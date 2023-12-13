3f5bdd4ac4bd6475698067f0f069d160492576b7
n0izestr3am <n0izestr3am@gmail.com>
2023-12-13 13:41:31 +0700

    |  2023-12-13-13:41:31

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
 
     public function index(Request $request){
 
-         $paging = $request->get('key');
+        $paging = $request->get('key') ?? 'buysbacks';
          //dd($paging);
         activity()->log(' '.auth()->user()->name.'Masuk ke halaman Dashoard ');
 
