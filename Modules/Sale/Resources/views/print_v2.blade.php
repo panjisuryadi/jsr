8a0b50847a8e8e306fc2de75bceeb603233b594b
n0izestr3am <n0izestr3am@gmail.com>
2023-12-22 11:34:34 +0700

    size print penjualan |  2023-12-22-11:34:34

--- Modules/Sale/Resources/views/print_v2.blade.php
+++ Modules/Sale/Resources/views/print_v2.blade.php
@@ -9,15 +9,22 @@
             margin: 0;
             padding: 0;
         }
+
+
+        @page {
+            size: 105mm 148.5mm; /* A4 size in millimeters */
+            margin: 3mm; /* 20mm margin for all sides */
+        }
+
         .invoice {
             width: 100%;
             margin: 0px auto;
 
-            padding: 12px;
+            padding: 6px;
         }
         .header {
             text-align: center;
-            margin-bottom: 30px;
+            margin-bottom: 10px;
             color: red; /* Change text color to red */
         }
         .invoice-details {
@@ -30,6 +37,14 @@
         }
         .invoice-items {
             margin-top: 0px;
+
+        }  
+
+        .fh {
+           font-size: 7pt !important;
+           line-height: 1;
+           text-align: center;
+
         }
         table {
             width: 100%;
@@ -44,20 +59,20 @@
             border: none;
 
         }
-
-
+  
         th, td {
             border: 1px solid red; /* Change border color to red */
-            padding: 8px;
+            padding: 2px;
             text-align: left;
-            font-size: 11pt !important;
+            font-size: 7pt !important;
             color: red; /* Change text color to red */
         }
         th{
             text-transform: uppercase;
+
         }
         .total {
-            margin-top: 20px;
+            margin-top: 2px;
             text-align: right;
             color: red; /* Change text color to red */
         }
@@ -68,30 +83,31 @@
 
 <div class="invoice-items">
 
-<table class="table1" style='width:100%; border: none !important; font-size:14pt; font-family:calibri; border-collapse: collapse;' border="0">
-    <td class="table1" width='60%' align='left' style='border: none !important;padding-right:80px; vertical-align:top'>
-        <span style='font-size:22pt'><b>{{ settings()->company_name }}</b></span></br>
+<table class="table1" style='width:100%; border: none !important; font-size:13pt; font-family:calibri; border-collapse: collapse;' border="0">
+    <td class="table1" width='60%' align='left' style='border: none !important;padding-right:60px; vertical-align:top'>
+        <span style='font-size:16pt'><b>{{ settings()->company_name }}</b></span></br>
          {{ settings()->company_email }}, {{ settings()->company_phone }}
                 <br>{{ settings()->company_address }}
     </td>
     <td style='border: none !important;vertical-align:top' width='40%' align='left'>
+        <br>
         Invoice: {{ $sale->reference }}</br>
         Tanggal : {{ \Carbon\Carbon::parse($sale->date)->format('d M, Y') }}</br>
         Kostumer: {{ $sale->customer_name }}</br>
         Alamat :  - </br>
     </td>
 </table>
-<p></p>
+<br>
 <table class="invoice-items">
     <thead>
-        <tr>
-            <th style="width:5%;" class ="text-center">No</th>
-            <th style="width:10%;" class ="text-center">Gambar</th>
-            <th style="width:5%;" class ="text-center">Jumlah</th>
-            <th class ="text-center">nama barang</th>
-            <th class ="text-center">kode</th>
-            <th class ="text-center">berat (gr)</th>
-            <th class ="text-center">Harga</th>
+        <tr >
+            <th style="width:5%;" class ="fh text-center">No</th>
+            <th style="width:10%;" class ="fh text-center">Gambar</th>
+            <th style="width:5%;" class ="fh text-center">Jumlah</th>
+            <th class ="fh text-center">barang</th>
+            <th class ="fh text-center">kode</th>
+            <th class ="fh text-center">berat <span style="font-size:4pt;">(gr)</span></th>
+            <th class ="fh text-center">Harga</th>
         </tr>
     </thead>
     <tbody>
@@ -108,7 +124,7 @@
 
 
 <td style="text-align:center;vertical-align:bottom">
-   <?php
+    <?php
     
     //$img = public_path('images/fallback_product_image.png')
     $image = $saleDetail->product->images;
@@ -120,13 +136,14 @@
 
     
     ?>
-     <img src="{{ $imagePath }}" order="0" width="70"/>
+     <img src="{{ $imagePath }}" order="0" width="40"/>
     
     
 </td>
 
                 <td style="text-align:center;vertical-align:bottom">
-                    {{ $saleDetail->quantity }}
+               
+                    {{ @$saleDetail->quantity }}
 
                 </td> 
 
@@ -183,35 +200,33 @@
 <hr>
 
 <div style="padding: 2px;color: red !important;">
-<p style="font-weight: bold;" class="bold">CATATAN</p>
+<div style="font-weight: bold;font-size: 9pt;" class="bold">CATATAN</div>
 
-<ol>
+<ol style="font-size:7pt;">
   <li>Mas dan Berat Barang telah di timbang dan disaksikan pembeli</li>
   <li>Barang ini dapat dijual kembali dengan potongan yang ditentukan</li>
   <li>Barang yang rusak terkena <strong>potongan hingga 30%</strong></li>
   <li>Bila dijual kembali harap membawa Surat</li>
-
-
 </ol>
 
 </div>
-        <table style='width:100%!important; font-size:12pt;' cellspacing='2'>
+        <table style='width:100%!important; font-size:11pt;' cellspacing='2'>
             <tr>
                 <td style="border: none !important;text-align: center;" align='center'>
                     Diterima Oleh,
                 </br></br>
                 </br></br>
                 </br></br>
-                <u>(..................................................)</u>
+                <u>(.............................................)</u>
                 </td>
                 <td style='border: none !important; padding:5px; text-align:left; width:30%'></td>
                 <td style="text-align: center; border: none !important;" align='center'>Hormat Kami,<br>
-                <span style='font-size:12pt'><b>{{ settings()->company_name }}</b></span></br>
+                <span style='font-size:9pt'><b>{{ settings()->company_name }}</b></span></br>
 
                 </br>
                 </br></br>
                 </br></br>
-                <u>(...................................................)</u>
+                <u>(..............................................)</u>
                 </td>
             </tr>
         </table>
