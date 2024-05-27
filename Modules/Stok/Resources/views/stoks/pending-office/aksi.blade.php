732336b974b6b132bf72a2a3ee13ba91c2d9df9e
Nofri26 <1zhal9991@gmail.com>
2024-05-27 14:53:19 +0700

    Fitur checkbox

--- Modules/Stok/Resources/views/stoks/pending-office/aksi.blade.php
+++ Modules/Stok/Resources/views/stoks/pending-office/aksi.blade.php
@@ -1,15 +1,15 @@
 <div class="text-center">
-    <a  class="btn btn-sm btn-info font-bold"
-        href="{{ route('stok.show_pending_office', ['categoryName' => $data->category_name, 'karatName' => $data->name]) }}">
+    <a class="btn btn-sm btn-info font-bold"
+       href="{{ route('stok.show_pending_office', ['categoryName' => $data->category_name, 'karatName' => $data->name]) }}">
         Detail
     </a>
 
-<button
-    onclick="process({{$data}})"
-    id="Status"
-    data-toggle="tooltip"
-     class="btn btn-sm btn-success font-bold">
-    Proses
-    </button>
+{{--    <button--}}
+{{--        onclick="process({{$data}})"--}}
+{{--        id="Status"--}}
+{{--        data-toggle="tooltip"--}}
+{{--        class="btn btn-sm btn-success font-bold">--}}
+{{--        Proses--}}
+{{--    </button>--}}
 
 </div>
