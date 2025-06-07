55e5eba76e6e313e15470d3c65e03ffdbb999c6b
user <user@MacBook-Pro-user.local>
2025-06-07 15:57:25 +0700

    update gedes

--- Modules/Product/Resources/views/products/modal/qrcode.blade.php
+++ Modules/Product/Resources/views/products/modal/qrcode.blade.php
@@ -12,11 +12,10 @@
           {!! \Milon\Barcode\Facades\DNS2DFacade::getBarCodeSVG($detail->product_code, 'QRCODE', 15, 15) !!}
         </div>
       </div>
-      <div class="col-6 flex justify-center items-center"> <!-- 👈 Make this a flex container -->
+      <div class="col-6 flex justify-center items-center ml-3"> <!-- 👈 Make this a flex container -->
     <div class="py-2 text-center font-semibold uppercase text-gray-600 no-underline text-lg hover:text-red-600 leading-tight" style="font-size: 55pt;">
       <p class=""><strong>{{ $detail->product_code }}</strong></p>
-      <p class=""><strong>{{ $detail->berat_emas }} gr</strong></p>
-      <p class=""><strong>{{ $detail->karat->name }}</strong></p>
+      <p class=""><strong>{{ $detail->berat_emas }} gr, {{ $detail->karat->name }}</strong></p>
     </div>
 </div>
     </div>
